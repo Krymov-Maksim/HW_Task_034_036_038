bf59e162dc86b681a175c8ea380bf044e6b595b7
Max_Kr <m.krymov@mail.ru>
2023-10-05 20:29:14 +0300

    HW_05_10_2023 ver.03

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 // // Напишите программу, которая покажет количество чётных чисел в массиве.
 // // [345, 897, 568, 234] -> 2
 
-// int[] CreateRandomArray(int NumArray)
+// int[] CreateRandomArray(int NumArray)        // Метод. Создает рандомный массив размером NumArray элеменов
 // {
 //     Console.Write("[");
 //     int[] array = new int[NumArray];
@@ -15,7 +15,7 @@
 //     return array;
 // }
 
-// void ShowArray(int[] array)
+// void ShowArray(int[] array)                  // Метод. Ищем четные элементы массива, складываем их в SumNumEven. Вывод на экран
 // {
 //     int SumNumEven = 0;
 //     for (int i = 0; i < array.Length; i++)
@@ -39,15 +39,12 @@
 
 
 
-
-
-
 // // Задача 36: Задайте одномерный массив, заполненный случайными числами. 
 // // Найдите сумму элементов, стоящих на нечётных позициях.
 // // [3, 7, 23, 12] -> 19
 // // [-4, -6, 89, 6] -> 0
 
-// int[] CreateRandomArray(int NumArray)
+// int[] CreateRandomArray(int NumArray)                    // Метод. Создает рандомный массив размером NumArray элеменов
 // {
 //     Console.Write("[");
 //     int[] array = new int[NumArray];
@@ -60,20 +57,19 @@
 //     return array;
 // }
 
-// void ShowArray(int[] array)
+// void ShowArray(int[] array)                              // Метод. Считаем сумму всех нечетных позиций. 
 // {
 //     int SumNumOdd = 0;
-//     for (int i = 0; i < array.Length; i++)
-//     {
-//         if (array[i] % 2 != 0)
+//     for (int i = 1; i < array.Length; i = i+2)
+//     {                        
 //             SumNumOdd = SumNumOdd + array[i];
 //     }
 //     Console.WriteLine($" -> {SumNumOdd}");
 // }
 
-// Console.WriteLine("How array do you need?");
+// Console.WriteLine("How array do you need?");            // Тело
 // int NumArray = Convert.ToInt32(Console.ReadLine());
-// if (NumArray < 1)
+// if (NumArray < 1)                                       // Количество элементов в массиве не должно быть меньше 1
 // {
 //     Console.WriteLine("Out of range! Need from more 0");
 // }
@@ -87,50 +83,32 @@
 
 
 
-// Задача 38: Задайте массив вещественных чисел. 
-// Найдите разницу между максимальным и минимальным элементов массива.
-// [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+// // Задача 38: Задайте массив вещественных чисел. 
+// // Найдите разницу между максимальным и минимальным элементов массива.
+// // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
 
 
-void[] DifferenceMaxMin(int NumArray)
-{
-    double min = NumArray
-    double[] array = new double[NumArray];
-    for (int i = 0; i < NumArray; i++)
-    {
-        array[i] = new Random().Next(-1000, 1000);
-        Console.Write($"{array[i]}, ");
-    }
-    Console.Write("]");
-    return array;
-}
+// void DiffMaxMin (double [] array)               // Метод определеющие минимум и максимиум в массиве. Считает разницу DIFF
+// {
+//     double min = array[0];
+//     double max = array[0];
 
-void ShowArray(int[] array)
-{
-    int SumNumOdd = 0;
-    for (int i = 0; i < array.Length; i++)
-    {
-        if (array[i] % 2 != 0)
-            SumNumOdd = SumNumOdd + array[i];
-    }
-    Console.WriteLine($" -> {SumNumOdd}");
-}
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] < min)   
+//             min = array[i];
+//         if (array[i] > max)   
+//             max = array[i];
+//     }        
+//     Console.WriteLine($"Difference between max {max} and {min} is {max - min} pounts.");
+// }
 
-Console.WriteLine("Вы можете поменять массив чисел руками в программе. Массив такой:");
-double[] array = {3.22, 4.2, 1.15, 77.15, 65.2};
-    for (int i = 0; i = array.Length; i++){
-        Console.WriteLine(array[i]);
-    }
+// Console.WriteLine("Вы можете поменять массив чисел руками в программе. Массив такой:");   // Тело
+// double[] array = {3.22, 4.2, 1.15, 77.15, 65.2};
+    
+//     for (int i = 0; i < array.Length; i++){                                      // Выводим массив на экран
+//         Console.Write(array[i] + " ");
+//     }
 
-
-int NumArray = Convert.ToInt32(Console.ReadLine());
-
-if (NumArray < 1)
-{
-    Console.WriteLine("Out of range! Need from more 0");
-}
-else
-    Console.WriteLine("How array do you need?");
-
-ShowArray(CreateRandomArray(NumArray));
+// DiffMaxMin(array);
 
